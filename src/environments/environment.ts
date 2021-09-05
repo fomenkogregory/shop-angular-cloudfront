1c8c939cfe44877acf290ec1cf3932e48265d7c3
fomenkogregory <fmk.g@ya.ru>
2021-09-05 21:12:19 +0300

    add real api call

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,14 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    products: 'https://cpikwrm5gl.execute-api.eu-west-1.amazonaws.com/dev',
+    order: 'https://cpikwrm5gl.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://cpikwrm5gl.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://cpikwrm5gl.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'https://cpikwrm5gl.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    products: true,
     order: false,
     import: false,
     bff: false,
